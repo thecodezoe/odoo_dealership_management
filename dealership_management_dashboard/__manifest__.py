b27ba4730e63c5db4f4b0ce8be29bf2489584217
Hiren-SerpentCS <hiren.pattani@serpentcs.com>
2020-10-02 23:26:39 +0530

    [ADD] dealership_management_dashbord: Added the Dealership Management Dashboard module

---
+++ dealership_management_dashboard/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dealership Management Dashboard",
+
+    'summary': """
+    Dealership Management,
+    Management,
+    Odoo Dealership Management,
+    Car Dealership Management,
+    Automobile Dealership Management,
+    Dealership Management Dashboard,
+    Odoo Dashboard,
+    Interactive Dashboard,
+    """,
+
+    'description': """
+        Dealership Management Dashboard.
+    """,
+
+    'author': "CodeZoe",
+    'website': "https://www.codezoe.com",
+
+    'category': 'Sales',
+    'version': '14.0.1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True
+}
