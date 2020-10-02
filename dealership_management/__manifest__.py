79995b31129e1e7841fcbbc5b856e38a4caaa693
Hiren Pattani <pattanihiren1@gmail.com>
2020-10-02 21:09:16 +0530

    [ADD] dealership_management: Added the dealership_management module.

---
+++ dealership_management/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dealership Management",
+
+    'summary': """
+    Dealership Management,
+    Management,
+    Odoo Dealership Management,
+    Car Dealership Management,
+    Automobile Dealership Management,
+    """,
+
+    'description': """
+        Dealership Management Module made with Odoo.
+    """,
+
+    'author': "CodeZoe",
+    'website': "https://www.codezoe.com",
+
+    'category': 'Sales',
+    'version': '14.0.1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True
+}
